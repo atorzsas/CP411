c24d6bf24302551cc49e045c4b4484757cf51c92
Reil4020 <148383972+Reil4020@users.noreply.github.com>
2023-11-29 15:40:34 -0500

    Update grid.hpp

--- grid.hpp
+++ grid.hpp
@@ -1,1 +1,19 @@
 //Nothing here just yet
+#pragma once
+
+#include <vector>
+#include <GL/glew.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+class Grid {
+public:
+    Grid(int slices);
+    void render();
+
+private:
+    GLuint vao;
+    GLuint length;
+    std::vector<glm::vec3> vertices;
+    std::vector<glm::uvec4> indices;
+};
