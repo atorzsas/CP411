4fdaadf12f9e1f5689ec6b82e02a1f02c17cc684
atorzsas <139482668+atorzsas@users.noreply.github.com>
2023-12-05 17:01:45 -0500

    Create menu.hpp

---
+++ menu.hpp
@@ -0,0 +1,25 @@
+#ifndef MENU_HPP_
+#define MENU_HPP_
+
+#include <GL/glut.h>
+
+// Define your menu action functions here
+void menu();
+void mainMenu(GLint option);
+void ObjSubMenu(GLint objectOption);
+void MCSTransMenu(GLint transOption);
+void WCSTransMenu(GLint transOption);
+void VCSTransMenu(GLint transOption);
+void reset();
+void MCSTransform(GLint);
+void WCSTransform(GLint);
+void VCSTransform(GLint);
+void cullMenu(GLint option);
+void lightMenu(GLint option);
+void lightTransform(GLint);
+void shadeMenu(GLint option);
+void animateMenu(GLint option);
+void curveSurfaceMenu(GLint option);
+void move();
+
+#endif // MENU_HPP_
