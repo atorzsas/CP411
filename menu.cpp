d5e8e712e6ee8627ca8660eacde34fc0581a38be
atorzsas <139482668+atorzsas@users.noreply.github.com>
2023-12-05 17:01:58 -0500

    Create menu.cpp

---
+++ menu.cpp
@@ -0,0 +1,34 @@
+#include "menu.hpp"
+
+// Define global variables and externs as needed
+// extern GLint csType; // Example extern variable
+
+void mainMenu(GLint option) {
+    switch (option) {
+        case 1:
+            // reset(); // Call your reset function
+            reset();
+            break;
+        case 2:
+            exit(0); // Quit the application
+            break;
+    }
+    glutPostRedisplay();
+}
+
+// Implement other menu functions below...
+// These functions should call the appropriate methods or set global state
+// variables based on the user's menu selections
+
+void ObjSubMenu(GLint objectOption) {
+    // ... Logic for object submenu ...
+}
+
+void MCSTransMenu(GLint transOption) {
+    // ... Logic for MCS transformation submenu ...
+}
+
+// ... Implement other menu functions as needed ...
+
+// Finally, ensure that you have implemented the extern variables and any other
+// required logic in your main application to support these menu actions.
