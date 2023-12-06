ebdf4b565389fb451d5a48d3d42ba8b6f9091ca5
atorzsas <139482668+atorzsas@users.noreply.github.com>
2023-12-05 23:11:02 -0500

    Update car.hpp

--- car.hpp
+++ car.hpp
@@ -1,9 +1,12 @@
 #ifndef CAR_HPP
 #define CAR_HPP
 
+#include <GL/glut.h>  // Include this if GLuint is not recognized
+
 class Car {
 public:
     Car(float startX, float startY, float startSpeed);
+    void setTexture(GLuint texID);  // Ensure parameter type is GLuint
     void draw();
     void update();
     void setSpeed(float newSpeed);
@@ -11,6 +14,7 @@ public:
 private:
     float x, y;
     float speed;
+    GLuint textureID;  // Make sure this is declared
 };
 
-#endif // CAR_H
+#endif // CAR_HPP
