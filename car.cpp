fd7f74d1bf7f5527beb430ef4e170b3f8ceb943b
atorzsas <139482668+atorzsas@users.noreply.github.com>
2023-12-05 23:10:50 -0500

    Update car.cpp

--- car.cpp
+++ car.cpp
@@ -5,14 +5,40 @@ Car::Car(float startX, float startY, float startSpeed) : x(startX), y(startY), s
     // Constructor implementation
 }
 
-void Car::draw() {
-    // Draw method implementation
-    glPushMatrix();
-    glColor3f(1.0, 0.0, 0.0); // Set color to red
-    glTranslatef(x, 0.2, y); // Translate to car's position
-    glutSolidCube(0.5); // Draw a cube representing the car
-    glPopMatrix();
+void Car::setTexture(GLuint texID) {
+    textureID = texID;
 }
+
+void Car::draw() {
+    glEnable(GL_TEXTURE_2D);
+    glBindTexture(GL_TEXTURE_2D, textureID);
+
+    glPushMatrix();
+    glColor3f(1.0, 1.0, 1.0); // Normally you'd use white to show the texture
+    glTranslatef(x, 0.2, y); // Translate to car's position
+
+    // Car's bottom
+    glBegin(GL_QUADS);
+    glTexCoord2f(0.0, 0.0); glVertex3f(-0.5, 0.0, -0.5); // Bottom Left
+    glTexCoord2f(1.0, 0.0); glVertex3f(0.5, 0.0, -0.5);  // Bottom Right
+    glTexCoord2f(1.0, 1.0); glVertex3f(0.5, 0.0, 0.5);   // Top Right
+    glTexCoord2f(0.0, 1.0); glVertex3f(-0.5, 0.0, 0.5);  // Top Left
+    glEnd();
+
+    // Car's top - adjust y value to raise the top
+    glBegin(GL_QUADS);
+    glTexCoord2f(0.0, 0.0); glVertex3f(-0.5, 1.0, -0.5); // Bottom Left
+    glTexCoord2f(1.0, 0.0); glVertex3f(0.5, 1.0, -0.5);  // Bottom Right
+    glTexCoord2f(1.0, 1.0); glVertex3f(0.5, 1.0, 0.5);   // Top Right
+    glTexCoord2f(0.0, 1.0); glVertex3f(-0.5, 1.0, 0.5);  // Top Left
+    glEnd();
+
+    // ... Add other sides of the car similarly with appropriate texture coordinates ...
+
+    glPopMatrix();
+    glDisable(GL_TEXTURE_2D);
+}
+
 
 void Car::update() {
     // Update method implementation
@@ -28,3 +54,4 @@ void Car::setSpeed(float newSpeed) {
     // setSpeed method implementation
     speed = newSpeed;
 }
+
